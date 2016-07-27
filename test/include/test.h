df053fd9f83f9c84f357bf689f2cc4b8b32a9382
Johnny Lee Othon <jleeothon@gmail.com>
2016-07-27 09:36:25 +0000

    Removed README, modified test, changes in generated files

--- test/include/test.h
+++ test/include/test.h
@@ -7,13 +7,13 @@
 #define TEST_H
 
 #include <corto/corto.h>
+#include <_interface.h>
+#include <corto/test/test.h>
+#include <antlr3/antlr3.h>
+
 #include <_type.h>
 #include <_api.h>
 #include <_meta.h>
-#include <_interface.h>
-
-#include <corto/test/test.h>
-#include <antlr3/antlr3.h>
 
 #ifdef __cplusplus
 extern "C" {
--- test/src/TestAntlr3.cpp
+++ test/src/TestAntlr3.cpp
@@ -19,12 +19,13 @@ corto_void _test_TestAntlr3_test(
     test_TestAntlr3 _this)
 {
 /* $begin(test/TestAntlr3/test) */
-    test_assert(1 == 1);
     pANTLR3_INPUT_STREAM input = antlr3StringStreamNew(
         (pANTLR3_UINT8)"HELLO WORLD",
         ANTLR3_ENC_UTF8,
         strlen("HELLO WORLD"),
         (pANTLR3_UINT8)"mock_input_file"
     );
+    test_assert(input != NULL);
+    input->close(input);
 /* $end */
 }
